54590442504865bb0d01bff41d1bc4782da3057d
DevinHanma <devinbc12@icloud.com>
2024-04-07 17:21:04 +0200

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Slot_Maschine
+
+## About
+This is a Node.js Project. I am coding a slot maschine to learn Javascript.
---
+++ SlotMaschine.js
@@ -0,0 +1,6 @@
+// 1. Depot some money
+// 2. Determine number of lines to bet on 
+// 3. Collect bet amount
+// 4. Spin slot maschine
+// 5. Check if user won
+// 6. Give winning amount
